519417f2d2043ca2ee6ce740872192c0167facb4
MerlinEgalite <egalite.merlin@gmail.com>
2022-06-15 11:53:08 +0200

    test: test setup draft + remove files

---
+++ test/compound/TestSetup.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity ^0.8.0;
+
+import "@morpho-contracts/test-foundry/compound/setup/TestSetup.sol" as TestSetupCompound;
+
+contract TestSetup is TestSetupCompound {
+    // TODO
+}
