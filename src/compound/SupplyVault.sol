c8d2dda87f3d3c781ed73666cb2609d533883fe0
Rubilmax <rmilon@gmail.com>
2022-06-28 11:11:53 +0200

    🔥 Remove local comp index

--- src/compound/SupplyVault.sol
+++ src/compound/SupplyVault.sol
@@ -15,7 +15,6 @@ contract SupplyVault is SupplyVaultUpgradeable {
 
     /// STORAGE ///
 
-    IComptroller.CompMarketState public localCompRewardsState; // The local rewards state.
     mapping(address => uint256) public userUnclaimedCompRewards; // The unclaimed rewards of the user.
     mapping(address => uint256) public compRewardsIndex; // The comp rewards index of the user.
 
@@ -38,16 +37,6 @@ contract SupplyVault is SupplyVaultUpgradeable {
     }
 
     /// EXTERNAL ///
-
-    /// @notice Returns the local rewards state.
-    /// @return The local rewards state.
-    function getLocalCompRewardsState()
-        external
-        view
-        returns (IComptroller.CompMarketState memory)
-    {
-        return localCompRewardsState;
-    }
 
     /// @notice Claims rewards from the underlying pool, swaps them for the underlying asset and supply them through Morpho.
     /// @return rewardsAmount The amount of rewards claimed, swapped then supplied through Morpho (in underlying).
@@ -92,56 +81,30 @@ contract SupplyVault is SupplyVaultUpgradeable {
     /// @notice Accrues unclaimed rewards for the cToken addresses and returns the total unclaimed rewards.
     /// @param _user The address of the user.
     function _accrueUserUnclaimedRewards(address _user) internal {
-        _updateRewardsIndex();
-        userUnclaimedCompRewards[_user] += _accrueCompRewards(
-            _user,
-            morpho.supplyBalanceInOf(address(poolToken), _user).onPool
+        address _poolTokenAddress = address(poolToken);
+        IComptroller.CompMarketState memory supplyState = comptroller.compSupplyState(
+            _poolTokenAddress
         );
-    }
 
-    /// @notice Updates supplier index and returns the accrued rewards of the supplier since the last update.
-    /// @param _user The address of the supplier.
-    /// @param _balance The user balance of tokens in the distribution.
-    /// @return accruedRewards The amount of accrued rewards.
-    function _accrueCompRewards(address _user, uint256 _balance)
-        internal
-        returns (uint256 accruedRewards)
-    {
-        uint256 currentRewardsIndex = localCompRewardsState.index;
+        uint256 deltaBlocks = block.number - supplyState.block;
+        uint256 supplySpeed = comptroller.compSupplySpeeds(_poolTokenAddress);
+
+        uint224 currentRewardsIndex;
+        if (deltaBlocks > 0 && supplySpeed > 0) {
+            uint256 supplyTokens = ICToken(_poolTokenAddress).totalSupply();
+            uint256 compAccrued = deltaBlocks * supplySpeed;
+            uint256 ratio = supplyTokens > 0 ? (compAccrued * 1e36) / supplyTokens : 0;
+
+            currentRewardsIndex = uint224(supplyState.index + ratio);
+        } else currentRewardsIndex = supplyState.index;
+
         uint256 userRewardsIndex = compRewardsIndex[_user];
         compRewardsIndex[_user] = currentRewardsIndex;
 
         if (userRewardsIndex != 0)
-            accruedRewards = (_balance * (currentRewardsIndex - userRewardsIndex)) / 1e36;
-    }
-
-    /// @notice Updates the rewards index.
-    function _updateRewardsIndex() internal {
-        IComptroller.CompMarketState memory _localCompRewardsState = localCompRewardsState;
-
-        if (_localCompRewardsState.block == block.number) return;
-        else {
-            address _poolTokenAddress = address(poolToken);
-            IComptroller.CompMarketState memory supplyState = comptroller.compSupplyState(
-                _poolTokenAddress
-            );
-
-            uint256 deltaBlocks = block.number - supplyState.block;
-            uint256 supplySpeed = comptroller.compSupplySpeeds(_poolTokenAddress);
-
-            uint224 newCompSupplyIndex;
-            if (deltaBlocks > 0 && supplySpeed > 0) {
-                uint256 supplyTokens = ICToken(_poolTokenAddress).totalSupply();
-                uint256 compAccrued = deltaBlocks * supplySpeed;
-                uint256 ratio = supplyTokens > 0 ? (compAccrued * 1e36) / supplyTokens : 0;
-
-                newCompSupplyIndex = uint224(supplyState.index + ratio);
-            } else newCompSupplyIndex = supplyState.index;
-
-            localCompRewardsState = IComptroller.CompMarketState({
-                index: newCompSupplyIndex,
-                block: CompoundMath.safe32(block.number)
-            });
-        }
+            userUnclaimedCompRewards[_user] +=
+                (morpho.supplyBalanceInOf(_poolTokenAddress, _user).onPool *
+                    (currentRewardsIndex - userRewardsIndex)) /
+                1e36;
     }
 }
