6a3a575175e0653a120ec2ae7e84b6d784ae7c24
MerlinEgalite <egalite.merlin@gmail.com>
2022-06-29 23:09:16 +0200

    🎨 Put gap in storage section
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- src/ERC4626Upgradeable.sol
+++ src/ERC4626Upgradeable.sol
@@ -49,6 +49,11 @@ abstract contract ERC4626Upgradeable is ERC20Upgradeable {
 
     ERC20 public asset; // The underlying asset used to supply through Morpho.
 
+    /// @dev This empty reserved space is put in place to allow future versions to add new
+    /// variables without shifting down storage in the inheritance chain.
+    /// See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+    uint256[49] private __gap;
+
     /// CONSTRUCTOR ///
 
     /// @notice Constructs the contract.
@@ -243,11 +248,4 @@ abstract contract ERC4626Upgradeable is ERC20Upgradeable {
         uint256 _amount,
         uint256 _shares
     ) internal virtual {}
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[49] private __gap;
 }
