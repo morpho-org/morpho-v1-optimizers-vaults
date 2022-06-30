2c2fa70c54bc725ba512f8956ebda3ff3bb49da5
MerlinEgalite <egalite.merlin@gmail.com>
2022-06-30 08:51:53 +0200

    🎨 For beauty
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- src/ERC4626Upgradeable.sol
+++ src/ERC4626Upgradeable.sol
@@ -11,8 +11,8 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 /// @custom:contact security@morpho.xyz
 /// @notice ERC4626 Tokenized Vault abstract upgradeable implementation, heavily inspired by Solmate's non-upgradeable implementation (https://github.com/Rari-Capital/solmate/blob/main/src/mixins/ERC4626.sol)
 abstract contract ERC4626Upgradeable is ERC20Upgradeable {
-    using SafeTransferLib for ERC20;
     using FixedPointMathLib for uint256;
+    using SafeTransferLib for ERC20;
 
     /// EVENTS ///
 
