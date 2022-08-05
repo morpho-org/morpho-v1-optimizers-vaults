c4f44ad617642164c1954e7271e68dec93414b80
Rubilmax <rmilon@gmail.com>
2022-08-05 09:18:43 +0200

    🔥 Remove duplicate declaration

--- test/compound/setup/TestSetupVaults.sol
+++ test/compound/setup/TestSetupVaults.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@tests/compound/setup/TestSetup.sol";
 
-import "@vaults/compound/SupplyHarvestVault.sol";
-import "@vaults/compound/SupplyVault.sol";
+import {SupplyHarvestVault} from "@vaults/compound/SupplyHarvestVault.sol";
+import {SupplyVault} from "@vaults/compound/SupplyVault.sol";
 
 import "@morpho-labs/morpho-utils/math/PercentageMath.sol";
 
