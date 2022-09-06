2db0fa498a8a80a6a683a842a60ae56f3703c63e
Rubilmax <rmilon@gmail.com>
2022-09-06 10:20:44 +0200

    Added optional endpoints

--- src/ERC4626UpgradeableSafe.sol
+++ src/ERC4626UpgradeableSafe.sol
@@ -31,4 +31,50 @@ abstract contract ERC4626UpgradeableSafe is ERC4626Upgradeable {
         // See: https://github.com/Rari-Capital/solmate/issues/178
         if (_initialDeposit > 0) deposit(_initialDeposit, address(this));
     }
+
+    /// EXTERNAL ///
+
+    /// @notice Deposits a given amount of underlying asset into the vault.
+    /// @param assets The amount of underlying asset to deposit. The caller must have approved this contract to spend this amount.
+    /// @return The amount of shares minted.
+    function deposit(uint256 assets) external returns (uint256) {
+        return deposit(assets, msg.sender);
+    }
+
+    /// @notice Mints a given amount of shares from the vault.
+    /// @param shares The amount of shares to mint. The caller must have approved this contract to spend the corresponding amount of underlying asset.
+    /// @return The amount of assets deposited.
+    function mint(uint256 shares) external returns (uint256) {
+        return deposit(shares, msg.sender);
+    }
+
+    /// @notice Withdraws a given amount of underlying asset from the vault.
+    /// @param assets The amount of underlying asset to withdraw.
+    /// @return The amount of shares withdrawn.
+    function withdraw(uint256 assets) external returns (uint256) {
+        return withdraw(assets, msg.sender, msg.sender);
+    }
+
+    /// @notice Withdraws a given amount of underlying asset from the vault and send them to `receiver`.
+    /// @param assets The amount of underlying asset to withdraw.
+    /// @param receiver The address of the receiver of the funds.
+    /// @return The amount of shares withdrawn.
+    function withdraw(uint256 assets, address receiver) public returns (uint256) {
+        return withdraw(assets, receiver, msg.sender);
+    }
+
+    /// @notice Redeems a given amount of shares from the vault.
+    /// @param shares The amount of shares to redeem.
+    /// @return The amount of assets withdrawn.
+    function redeem(uint256 shares) external returns (uint256) {
+        return redeem(shares, msg.sender, msg.sender);
+    }
+
+    /// @notice Redeems a given amount of shares from the vault and send them to `receiver`.
+    /// @param shares The amount of shares to redeem.
+    /// @param receiver The address of the receiver of the funds.
+    /// @return The amount of assets withdrawn.
+    function redeem(uint256 shares, address receiver) public returns (uint256) {
+        return redeem(shares, receiver, msg.sender);
+    }
 }
