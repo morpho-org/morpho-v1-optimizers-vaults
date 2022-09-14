e02426f5902c12374608c4e28feb1c5ee3ddbd94
MerlinEgalite <egalite.merlin@gmail.com>
2022-09-14 15:15:28 +0200

    📌 Change solc constraint for interfaces

--- src/compound/interfaces/ISupplyVault.sol
+++ src/compound/interfaces/ISupplyVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GNU AGPLv3
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
 import {IERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC4626Upgradeable.sol";
 
