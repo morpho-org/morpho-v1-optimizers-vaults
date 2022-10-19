5cf4212580bb03d86810a7604b6b69a1a249e3ad
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-19 17:26:14 +0200

    ✅ Add testing case

--- test/compound/TestSupplyVault.t.sol
+++ test/compound/TestSupplyVault.t.sol
@@ -354,4 +354,28 @@ contract TestSupplyVault is TestSetupVaults {
         assertEq(token.balanceOf(address(daiSupplyVault)), _deal - _toTransfer);
         assertEq(token.balanceOf(_to), _toTransfer);
     }
+
+    function testAccrueRewardsToCorrectUser() public {
+        uint256 amount = 1e6 ether;
+
+        vm.startPrank(address(supplier1));
+        ERC20(dai).approve(address(daiSupplyVault), type(uint256).max);
+        daiSupplyVault.deposit(amount, address(supplier1));
+
+        vm.roll(block.number + 1000);
+
+        daiSupplyVault.redeem(
+            daiSupplyVault.balanceOf(address(supplier1)),
+            address(supplier2),
+            address(supplier1)
+        );
+        vm.stopPrank();
+
+        // Balance of supplier1 is expected to be 0.
+        assertEq(daiSupplyVault.balanceOf(address(supplier1)), 0);
+
+        // supplier1 must have some rewards to claim.
+        (, uint256 unclaimed) = daiSupplyVault.userRewards(address(supplier1));
+        assertGt(unclaimed, 0);
+    }
 }
