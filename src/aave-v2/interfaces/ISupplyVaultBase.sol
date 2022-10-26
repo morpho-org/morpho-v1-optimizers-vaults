b6b8a5e7b7b838f0fd3aaac0c1368d00e18241fe
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-26 11:53:57 +0200

    🧑‍💻 (#173) Improve Aave V2 interfaces

---
+++ src/aave-v2/interfaces/ISupplyVaultBase.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity >=0.8.0;
+
+import {IERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC4626Upgradeable.sol";
+import {IMorpho} from "@contracts/aave-v2/interfaces/IMorpho.sol";
+
+interface ISupplyVaultBase is IERC4626Upgradeable {
+    function morpho() external view returns (IMorpho);
+
+    function poolToken() external view returns (address);
+
+    function transferTokens(
+        address _asset,
+        address _to,
+        uint256 _amount
+    ) external;
+}
