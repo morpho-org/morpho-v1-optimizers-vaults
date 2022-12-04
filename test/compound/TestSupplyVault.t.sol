9870cd86030c651e52e8afce2d496ec543761e7b
patrick <patrick@morpho.xyz>
2022-12-04 18:38:03 -0500

    Clarify comments and add more tests

--- test/compound/TestSupplyVault.t.sol
+++ test/compound/TestSupplyVault.t.sol
@@ -677,12 +677,13 @@ contract TestSupplyVault is TestSetupVaults {
         uint256 balanceBefore1 = ERC20(dai).balanceOf(address(vaultSupplier1));
         uint256 balanceBefore2 = ERC20(dai).balanceOf(address(vaultSupplier2));
 
-        // This should use predicted update indexes.
+        // This should test that using the lens' predicted indexes is the correct amount to use.
         uint256 preview1 = daiSupplyVault.previewMint(amount);
         vaultSupplier1.mintVault(daiSupplyVault, amount);
         assertEq(preview1, balanceBefore1 - ERC20(dai).balanceOf(address(vaultSupplier1)));
 
-        // The indexes should be the same as before this call.
+        // The mint interacts with Morpho which updates the indexes,
+        // so this should test that the lens predicted index does not differ from Morpho's actual index
         uint256 preview2 = daiSupplyVault.previewMint(amount);
         vaultSupplier2.mintVault(daiSupplyVault, amount);
         assertEq(preview2, balanceBefore2 - ERC20(dai).balanceOf(address(vaultSupplier2)));
@@ -691,14 +692,59 @@ contract TestSupplyVault is TestSetupVaults {
     function testPreviewDeposit() public {
         uint256 amount = 1e5 ether;
 
-        // This should use predicted update indexes.
-        uint256 previewBefore = daiSupplyVault.previewDeposit(amount);
+        // This should test that using the lens' predicted indexes is the correct amount to use.
+        uint256 preview1 = daiSupplyVault.previewDeposit(amount);
         vaultSupplier1.depositVault(daiSupplyVault, amount);
-        assertEq(daiSupplyVault.balanceOf(address(vaultSupplier1)), previewBefore, "before");
+        assertEq(daiSupplyVault.balanceOf(address(vaultSupplier1)), preview1, "before");
 
-        // The indexes should be the same as before this call.
-        uint256 previewAfter = daiSupplyVault.previewDeposit(amount);
+        // The mint interacts with Morpho which updates the indexes,
+        // so this should test that the lens predicted index does not differ from Morpho's actual index
+        uint256 preview2 = daiSupplyVault.previewDeposit(amount);
         vaultSupplier2.depositVault(daiSupplyVault, amount);
-        assertEq(daiSupplyVault.balanceOf(address(vaultSupplier2)), previewAfter, "after");
+        assertEq(daiSupplyVault.balanceOf(address(vaultSupplier2)), preview2, "after");
+    }
+
+    function testPreviewWithdraw() public {
+        uint256 amount = 1e5 ether;
+
+        vaultSupplier1.depositVault(daiSupplyVault, amount * 2);
+        vaultSupplier2.depositVault(daiSupplyVault, amount * 2);
+
+        uint256 balanceBefore1 = daiSupplyVault.balanceOf(address(vaultSupplier1));
+        uint256 balanceBefore2 = daiSupplyVault.balanceOf(address(vaultSupplier2));
+
+        vm.roll(block.number + 100);
+
+        // This should test that using the lens' predicted indexes is the correct amount to use.
+        uint256 preview1 = daiSupplyVault.previewWithdraw(amount);
+        vaultSupplier1.withdrawVault(daiSupplyVault, amount);
+        assertEq(preview1, balanceBefore1 - daiSupplyVault.balanceOf(address(vaultSupplier1)));
+
+        // The mint interacts with Morpho which updates the indexes,
+        // so this should test that the lens predicted index does not differ from Morpho's actual index
+        uint256 preview2 = daiSupplyVault.previewWithdraw(amount);
+        vaultSupplier2.withdrawVault(daiSupplyVault, amount);
+        assertEq(preview2, balanceBefore2 - daiSupplyVault.balanceOf(address(vaultSupplier2)));
+    }
+
+    function testPreviewRedeem() public {
+        uint256 amount = 1e5 ether;
+
+        vaultSupplier1.depositVault(daiSupplyVault, amount * 2);
+        vaultSupplier2.depositVault(daiSupplyVault, amount * 2);
+
+        uint256 balanceBefore1 = ERC20(dai).balanceOf(address(vaultSupplier1));
+        uint256 balanceBefore2 = ERC20(dai).balanceOf(address(vaultSupplier2));
+
+        // This should test that using the lens' predicted indexes is the correct amount to use.
+        uint256 preview1 = daiSupplyVault.previewRedeem(amount);
+        vaultSupplier1.redeemVault(daiSupplyVault, amount);
+        assertEq(balanceBefore1 + preview1, ERC20(dai).balanceOf(address(vaultSupplier1)));
+
+        // The mint interacts with Morpho which updates the indexes,
+        // so this should test that the lens predicted index does not differ from Morpho's actual index
+        uint256 preview2 = daiSupplyVault.previewRedeem(amount);
+        vaultSupplier2.redeemVault(daiSupplyVault, amount);
+        assertEq(balanceBefore2 + preview2, ERC20(dai).balanceOf(address(vaultSupplier2)));
     }
 }
