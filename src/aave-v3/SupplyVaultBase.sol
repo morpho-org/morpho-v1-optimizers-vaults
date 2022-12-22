55546604f04bebcc14b62ce5f624d4f60346a751
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-22 17:32:49 +0100

    refactor: update gaps

--- src/aave-v3/SupplyVaultBase.sol
+++ src/aave-v3/SupplyVaultBase.sol
@@ -212,5 +212,5 @@ abstract contract SupplyVaultBase is ISupplyVaultBase, ERC4626UpgradeableSafe, O
     /// @dev This empty reserved space is put in place to allow future versions to add new
     /// variables without shifting down storage in the inheritance chain.
     /// See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-    uint256[48] private __gap;
+    uint256[49] private __gap;
 }
