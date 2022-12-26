9a55fc8e860d73da3a2f57d23a9d28d3ec6f70b8
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-26 19:38:13 +0100

    test: fuzzing caller address

--- test/compound/TestSupplyVaultBase.t.sol
+++ test/compound/TestSupplyVaultBase.t.sol
@@ -4,17 +4,9 @@ pragma solidity ^0.8.0;
 import "./setup/TestSetupVaults.sol";
 
 contract TestSupplyVaultBase is TestSetupVaults {
-    function testShouldTransferRewardsToRecipient(uint256 _amount) public {
+    function testShouldTransferRewardsToRecipient(address _caller, uint256 _amount) public {
         vm.assume(_amount > 0);
-        _prepareTransfer(_amount);
 
-        daiSupplyVault.transferRewards();
-
-        assertEq(ERC20(MORPHO_TOKEN).balanceOf(address(daiSupplyVault)), 0);
-        assertEq(ERC20(MORPHO_TOKEN).balanceOf(RECIPIENT), _amount);
-    }
-
-    function _prepareTransfer(uint256 _amount) internal {
         assertEq(ERC20(MORPHO_TOKEN).balanceOf(RECIPIENT), 0);
 
         deal(MORPHO_TOKEN, address(daiSupplyVault), _amount);
@@ -23,5 +15,11 @@ contract TestSupplyVaultBase is TestSetupVaults {
         // Allow the vault to transfer rewards.
         vm.prank(MORPHO_DAO);
         IRolesAuthority(MORPHO_TOKEN).setUserRole(address(daiSupplyVault), 0, true);
+
+        vm.prank(_caller);
+        daiSupplyVault.transferRewards();
+
+        assertEq(ERC20(MORPHO_TOKEN).balanceOf(address(daiSupplyVault)), 0);
+        assertEq(ERC20(MORPHO_TOKEN).balanceOf(RECIPIENT), _amount);
     }
 }
